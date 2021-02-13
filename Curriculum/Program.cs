ae670e4ff35516377b89d32eaf4ff3b18161f1f1
Cengiz Barkın Öztürk <cengizbarkin@gmail.com>
2021-02-13 19:10:48 +0300

    Initial check-in of module Curriculum

---
+++ Curriculum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curriculum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
